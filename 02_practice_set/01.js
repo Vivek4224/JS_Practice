43d191e457b8d8cf5b8b43524c1a010228c7a946
Vivek4224 <vivekbarvaliya4224@gmail.com>
2024-11-08 18:59:26 +0530

    practice set 2

---
+++ 02_practice_set/01.js
@@ -0,0 +1,5 @@
+let dal = 100
+let roti = 10
+let ice_cream = 30
+
+console.log('₹'+((1 * dal) + (2 * roti) + (1 * ice_cream)))
---
+++ 02_practice_set/02.js
@@ -0,0 +1,6 @@
+iphone = 79990
+airpods = 24990
+
+final_bill = "payable gst is" + "₹" + ((1 * iphone) + (2 * airpods)) * 18/100 ;
+
+console.log(final_bill)
